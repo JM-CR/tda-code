e7ef18347c7565cd77a1f995f6e6680ecf7d7b19
Josue Mosh <msi_98_98@yahoo.com.mx>
2020-02-17 15:13:24 -0600

    Initial template for the Euler's model

---
+++ P1/model.h
@@ -0,0 +1,25 @@
+// Euler approximation to solve ordinary differential equations.
+// Author: Josue Mosiah Contreras Rocha
+// File: model.h
+// Date: 17/02/20
+
+// -----------------------------
+// System headers
+// -----------------------------
+
+
+// -----------------------------
+// Public interface
+// -----------------------------
+
+/* Constants */
+
+/* Types declarations */
+
+/* Global variables */
+
+/* Function prototypes */
+
+/**
+ *
+ */
