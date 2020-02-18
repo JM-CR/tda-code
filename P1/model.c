e7099cc1b2e9e0380254d0ce4d0230327cea8f8f
Josue Mosh <msi_98_98@yahoo.com.mx>
2020-02-17 19:35:58 -0600

    Model's public interface and initial approximation Yn

--- P1/model.c
+++ P1/model.c
@@ -13,15 +13,29 @@
 // Private elements
 // -----------------------------
 
-// Private global variables
-
-/* Private macros and constants */
-
-/* Private types */
-
 /* Private global variables */
+static size_t sampleSize;
 
 /* Private functions */
+
+/**
+ * Calculates the approximation 'Yn'.
+ *
+ * @param coefficients EDO's coefficients starting with the highest derivative.
+ * @param initialValues Initial conditions at given time.
+ * @param order Exponent of the greatest derivative.
+ * @param step Time to calculate.
+ * @return Approximate point Yn.
+ */
+static double getPoint( double *coefficients, double* initialValues, size_t order, double step ) {
+	// Cumulative calculations
+	double sum = initialValues[order - 1];
+	for ( unsigned int i = 0; i < order; ++i )
+		sum -= coefficients[i] * initialValues[i];
+
+	// Result
+	return initialValues[0] + step * sum;
+}
 
 
 // -----------------------------
@@ -29,3 +43,11 @@
 // -----------------------------
 
 /* Implementation of the public functions */
+
+void setSampleSize( unsigned int size ) {
+	sampleSize = size;
+}
+
+double *processData( double *steps, double *coefficients, double *initialValues, size_t order ) {
+	
+}
--- P1/model.h
+++ P1/model.h
@@ -1,4 +1,6 @@
-// Euler approximation to solve ordinary differential equations.
+// Utilities to implement the Euler approximation.
+// The term Ordinary Differential Equation will be abbreviated as EDO.
+//
 // Author: Josue Mosiah Contreras Rocha
 // File: model.h
 // Date: 17/02/20
@@ -21,5 +23,21 @@
 /* Function prototypes */
 
 /**
+ * Sets the number of desire results for the Euler's method.
+ * This function must be called before processData().
  *
+ * @param size Number of results to process.
  */
+void setSampleSize( unsigned int size );
+
+/**
+ * Solves an EDO using the Euler's method.
+ * The number of results depend on the previous call of setSampleSize().
+ *
+ * @param steps Times to traverse.
+ * @param coefficients EDO's coefficients starting with the highest derivative.
+ * @param initialValues Initial conditions for the EDO.
+ * @param order Exponent of the greatest derivative.
+ * @return Results at each step.
+ */
+double *processData( double *steps, double *coefficients, double *initialValues, size_t order );
